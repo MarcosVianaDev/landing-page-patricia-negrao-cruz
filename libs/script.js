8cf7053ba7fa49f6a1bb8dd45cd33d23fed9b28e
Marcos Viana <97001316+Marcosviana91@users.noreply.github.com>
2023-10-08 11:44:51 -0300

    aplicando estruturação de projeto +btn whatsapp

--- libs/script.js
+++
@@ -1,9 +0,0 @@
-import { } from "./jquery/jquery-3.7.0.min.js";
-
-// $(function(){
-//     fetch('https://api.linkedin.com/v2/me')
-//     .then(res => res.json())
-//     .then(res => {
-//         console.log(res);
-//     });
-// });
